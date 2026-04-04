3f167c02d7d99ae1a6954824a25ae1eae45be979
PanshoOw <programacion.frvillarzu@gmail.com>
2026-04-04 19:29:12 -0300

    Implementa facturacion, Docker y configuracion SonarQube/Jenkins

---
+++ src/main/java/com/duoc/backend/Invoice/AdditionalChargeRequest.java
@@ -0,0 +1,23 @@
+package com.duoc.backend.Invoice;
+
+public class AdditionalChargeRequest {
+
+    private String description;
+    private Double amount;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
