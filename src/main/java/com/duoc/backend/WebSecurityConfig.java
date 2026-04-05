c976aea8ea3c69613e26917189124d97a600c902
PanshoOw <programacion.frvillarzu@gmail.com>
2026-04-04 21:56:47 -0300

    Corrige WebSecurityConfig inyectando filtro en configure

--- src/main/java/com/duoc/backend/WebSecurityConfig.java
+++ src/main/java/com/duoc/backend/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.duoc.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,23 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity()
+@EnableWebSecurity
 @Configuration
-class WebSecurityConfig{
-
-    @Autowired
-    JWTAuthorizationFilter jwtAuthorizationFilter;
+class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(
+            HttpSecurity http,
+            JWTAuthorizationFilter jwtAuthorizationFilter) throws Exception {
 
         http
-                .csrf((csrf) -> csrf
-                        .disable())
-                .authorizeHttpRequests( authz -> authz
-                        .requestMatchers(HttpMethod.POST,Constants.LOGIN_URL).permitAll()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, Constants.LOGIN_URL).permitAll()
                         .anyRequest().authenticated())
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
